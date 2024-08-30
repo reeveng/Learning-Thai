4739795619af9ca6b50212f4bebc885583b54878
J Mad <36441093+reeveng@users.noreply.github.com>
2024-08-30 20:58:34 +0000

    Update alphabet.ts

--- src/data/alphabet.ts
+++ src/data/alphabet.ts
@@ -7,10 +7,40 @@ export type AlphabetEntry = {
 
 export const translations: AlphabetEntry[] = [
 	{
-		letter: '',
-		pronounciation: '',
+		letter: 'ด',
+		pronounciation: 'd',
 		explanation: '',
-		example: ''
+		example: 'danger'
+	},
+	{
+		letter: 'ส',
+		pronounciation: 's',
+		explanation: '',
+		example: 'silver'
+	},
+	{
+		letter: 'ท',
+		pronounciation: 'th',
+		explanation: '',
+		example: 'tongue'
+	},
+	{
+		letter: 'อ',
+		pronounciation: 'a',
+		explanation: '',
+		example: 'aluminium'
+	},
+	{
+		letter: 'ว',
+		pronounciation: 'wh',
+		explanation: '',
+		example: 'when'
+	},
+	{
+		letter: 'บ',
+		pronounciation: 'b',
+		explanation: '',
+		example: 'boobs'
 	}
 ];
 
